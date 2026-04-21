357ce27c6ed4dcf52b112457608dc43b5268a238
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-04-21 20:47:45 +0300

    Add ShopDB schema: create tables and foreign keys

--- task.sql
+++ task.sql
@@ -1,1 +1,36 @@
 # Write your SQL code for the database creation here. Good luck! 
+CREATE DATABASE ShopDB;
+USE ShopDB;
+
+CREATE TABLE Products (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(50),
+  Description VARCHAR(100),
+  Price INT,
+  WarehouseAmount INT
+);
+
+
+CREATE TABLE Customers(
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  FirstName VARCHAR(50),
+  LastName VARCHAR(100),
+  Email VARCHAR(100),
+  Address VARCHAR(50)
+);
+
+
+CREATE TABLE Orders(
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  OrderID INT NULL,
+  ProductID INT NULL
+);
+
+CREATE TABLE OrderItems (
+ID INT AUTO_INCREMENT PRIMARY KEY,
+OrderID INT NULL,
+ProductID INT NULL,
+FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+);
+
